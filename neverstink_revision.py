3aee97521c7f01fa83a599868791348953eafa9e
d-altieri <140778442+d-altieri@users.noreply.github.com>
2023-09-12 13:19:05 -0500

    File for training and NeverStink location change

--- neverstink_revision.py
+++ neverstink_revision.py
@@ -1,6 +1,6 @@
-filter2 = open(r'C:\Users\daltier1\OneDrive - T-Mobile USA\Visual Studio\python\UpdatedNeverStink1.2.txt' , 'w')
+filter2 = open(r'C:\Users\danie\OneDrive\Visual Studio\learning-python\UpdatedNeverStink1.3.txt' , 'w')
 
-with open(r'C:\Users\daltier1\OneDrive - T-Mobile USA\Visual Studio\python\NeverStink1.2.txt', 'r') as filter1:
+with open(r'C:\Users\danie\OneDrive\Visual Studio\learning-python\NeverStink1.3.txt', 'r') as filter1:
     for line in filter1:
         if line.startswith('#'):
             continue
