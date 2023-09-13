fec2f2dd58723556e66a42c2fdae36864fbd423a
d-altieri <140778442+d-altieri@users.noreply.github.com>
2023-09-12 21:47:56 -0500

    directory address change

--- neverstink_revision.py
+++ neverstink_revision.py
@@ -1,6 +1,6 @@
-filter2 = open(r'C:\Users\danie\OneDrive\Visual Studio\learning-python\UpdatedNeverStink1.3.txt' , 'w')
+filter2 = open(r'C:\Users\danie\OneDrive\vscode\learning-python\UpdatedNeverStink1.3.txt' , 'w')
 
-with open(r'C:\Users\danie\OneDrive\Visual Studio\learning-python\NeverStink1.3.txt', 'r') as filter1:
+with open(r'C:\Users\danie\OneDrive\vscode\learning-python\NeverStink1.3.txt', 'r') as filter1:
     for line in filter1:
         if line.startswith('#'):
             continue
