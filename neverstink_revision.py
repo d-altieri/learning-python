6b9fd642d90d7c454c39634dfbfe24ab19e90538
d-altieri <140778442+d-altieri@users.noreply.github.com>
2023-09-14 11:24:07 -0500

    donut revision

--- neverstink_revision.py
+++ neverstink_revision.py
@@ -1,8 +1,9 @@
 filter2 = open(r'C:\Users\danie\OneDrive\vscode\learning-python\UpdatedNeverStink1.3.txt' , 'w')
+output = []
 
 with open(r'C:\Users\danie\OneDrive\vscode\learning-python\NeverStink1.3.txt', 'r') as filter1:
     for line in filter1:
-        if line.startswith('#'):
-            continue
-        else:
-            filter2.writelines([line])
+        if not line.startswith('#') and line.strip():
+            output.append(line)
+
+filter2.writelines(output)
