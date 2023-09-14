f7f5dba5ef07f6c3b714a9e722d1ffefe965c02f
d-altieri <140778442+d-altieri@users.noreply.github.com>
2023-09-14 12:47:07 -0500

    Rules separated with a space

--- neverstink_revision.py
+++ neverstink_revision.py
@@ -1,8 +1,12 @@
 filter2 = open(r'C:\Users\danie\OneDrive\vscode\learning-python\UpdatedNeverStink1.3.txt' , 'w')
 output = []
+rules = ("Show" , "Hide")
+
 
 with open(r'C:\Users\danie\OneDrive\vscode\learning-python\NeverStink1.3.txt', 'r') as filter1:
     for line in filter1:
+        if line.startswith(rules):
+            output.append("\n")
         if not line.startswith('#') and line.strip():
             output.append(line)
 
