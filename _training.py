f6544af78d97690a69e47a4f4ad7f28042752c91
d-altieri <140778442+d-altieri@users.noreply.github.com>
2023-09-17 11:04:08 -0500

    training and file name changes

--- _training.py
+++ _training.py
@@ -1,6 +1,3 @@
-numbers = [1 , 2 , 3 , 4 , 5]
-words = ["how" , "now" , "brown" , "cow"]
+search = input("How does this work?")
 
-test = open(r'C:\Users\danie\OneDrive\Visual Studio\learning-python\UpdatedNeverStink1.3.txt' , 'w')
-
-test.write(str(words))
+print(search)
--- scraping.py
+++
@@ -1,9 +0,0 @@
-import urllib3
-
-http = urllib3.PoolManager()
-
-resp = http.request("GET", "https://www.poewiki.net/wiki/Path_of_Exile_Wiki")
-web_data = []
-x = resp.data.find("div")
-
-print(x)
