5d37ee3b2ef958afd18711b72dc32fb5da969226
d-altieri <daniel.j.altieri@gmail.com>
2023-10-28 21:00:29 -0500

    Nested loops

--- _training.py
+++ _training.py
@@ -1,3 +1,13 @@
-search = input("How does this work?")
+'''
+outer_loop = ["x", "y", "z"]
+inner_loop = [1, 2, 3]
 
-print(search)
+for i in outer_loop:
+    for j in inner_loop:
+        print(i, j)
+'''
+
+outer_loop = ["x", "y", "z"]
+
+for i in outer_loop:
+    print(i)
