5688214c9550891f3afb5c23673d87e238f76d24
d-altieri <daniel.j.altieri@gmail.com>
2024-03-21 21:34:33 -0500

    Original omnibus template - Approved

---
+++ omnibus_template.py
@@ -0,0 +1,45 @@
+### Omnibus maintenance template ###
+
+number_of_sites = int(input('How many hub sites? '))
+
+output = []
+nest_output = []
+
+while number_of_sites > 0:
+    hubs = input('Enter Hub ID: ').upper()
+    sectors = input('Enter affected sectors: ').split(' ')
+    output.append(f'{hubs}')
+    for sector in sectors:
+        output.append(f'{hubs}_{sector}1LAB - 2100 | 1900 | LAA')
+        nest_output.append(f'{hubs}_{sector}1LAB')
+        continue
+    output.append('')
+    number_of_sites -= 1
+
+print('''
+
+
+===============================================
+=================  FOR SLACK  =================
+===============================================
+
+
+PoC:
+
+
+Alarms:
+
+
+Following sectors and frequencies have been locked:
+''')
+
+print(*output, sep='\n')
+
+print('''
+
+
+==============================================
+=================  FOR NEST  =================
+==============================================
+''')
+print(*nest_output, sep='\n')
