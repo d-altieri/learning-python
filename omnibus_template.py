74fdd513ab7c998f84ca8364aace87ebd19acd67
d-altieri <daniel.j.altieri@gmail.com>
2024-04-04 16:45:43 -0500

    Removed sectors from nest_output

--- omnibus_template.py
+++ omnibus_template.py
@@ -9,18 +9,28 @@ while number_of_sites > 0:
     hubs = input('Enter Hub ID: ').upper()
     sectors = input('Enter affected sectors: ').split(' ')
     output.append(f'{hubs}')
+    nest_output.append(f'{hubs}')
     for sector in sectors:
         output.append(f'{hubs}_{sector}1LAB - 2100 | 1900 | LAA')
-        nest_output.append(f'{hubs}_{sector}1LAB')
         continue
     output.append('')
     number_of_sites -= 1
+
+
+print('''
+
+
+==============================================
+=================    NEST    =================
+==============================================
+''')
+print(*nest_output, sep='\n')
 
 print('''
 
 
 ===============================================
-=================  FOR SLACK  =================
+================   HANDOVER   =================
 ===============================================
 
 
@@ -34,12 +44,3 @@ Following sectors and frequencies have been locked:
 ''')
 
 print(*output, sep='\n')
-
-print('''
-
-
-==============================================
-=================  FOR NEST  =================
-==============================================
-''')
-print(*nest_output, sep='\n')
