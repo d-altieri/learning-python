e95c574638670c32e8658e84e22bb8ab5f397dd7
d-altieri <daniel.j.altieri@gmail.com>
2024-04-10 22:59:12 -0500

    'findall' added and 'print' changed

--- using_re.py
+++ using_re.py
@@ -5,9 +5,9 @@ text = '''
 
 lines = text.splitlines()
 pattern = r"\w{8}_\d{2}LAB"
-match = re.search(pattern, text)
+match = re.findall(pattern, text)
 
 if match:
-    print(match.group())
+    print(*match, sep="\n")
 else:
     print("No equipment found")
