5c5cc06959bf18bc6df9089eea9ee13f73f583d2
d-altieri <daniel.j.altieri@gmail.com>
2024-04-16 10:00:53 -0500

    Editing out data from input

--- using_re.py
+++ using_re.py
@@ -1,13 +1,14 @@
 import re
 
-text = '''
+text = '''skdjghaskjghla03453ba1230-93905u2345
 '''
 
 lines = text.splitlines()
 pattern = r"\w{8}_\d{2}LAB"
 match = re.findall(pattern, text)
+result = sorted(match)
 
 if match:
-    print(*match, sep="\n")
+    print(*result, sep="\n")
 else:
     print("No equipment found")
